c76d78960fef8b0325121b94076bd3f7aa6e6df9
sweetsica <barondeepsnow@gmail.com>
2022-12-09 14:26:00 +0700

    new branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,12 +52,17 @@
                 <div class="p-6">
                     <div class="flex items-center">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                        <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a></div>
+                        <div class="ml-4 text-lg leading-7 font-semibold">
+                            <a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a>
+                        </div>
+                        <div class="ml-4 text-lg leading-7 font-semibold">
+                            <a href="{{route('task.reset')}}" class="underline text-gray-900 dark:text-white">Reset nhiệm vụ</a>
+                        </div>
                     </div>
 
                     <div class="ml-12">
                         <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                            Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                            Bộ quản lý thông tin nhân sự, thông tin khách hàng và định mức lao động áp dụng cho MTT và TBHT được tạo bởi ngôn ngữ Laravel. <br>Made by Sweetsica!💕
                         </div>
                     </div>
                 </div>
@@ -128,25 +133,26 @@
                         <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
 
-                    <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-                        Shop
+                    <a href="https://zalo.me/0327350489" class="ml-1 underline">
+                        Liên hệ zalo
                     </a>
 
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
                         <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                     </svg>
 
-                    <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                        Sponsor
+                    <a href="barondeepsnow@gmail.com" class="ml-1 underline">
+                        barondeepsnow@gmail.com
                     </a>
                 </div>
             </div>
-
             <a class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                 <a href="{{route('logout')}}" style="color: white">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </a>
+            </a>
         </div>
+    </div>div>
     </div>
 </div>
 </div>
