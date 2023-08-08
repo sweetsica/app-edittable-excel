d22f03983449a568880d49d4baf09d0068529cb4
Snow Deep <barondeepsnow@gmail.com>
2023-08-08 15:02:20 +0700

    add chmod

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -25,8 +25,9 @@ class ReportController extends Controller
         $name_file = $file->getClientOriginalName();
         $date = Carbon::today()->format('d-m-Y');
         $path = Storage::putFileAs("public/report/".'09-09-09',$request->file('files'),$name_file);
-        $link_file = URL::to('/').Storage::url('report/'.$date.'/'.$name_file);
-        Storage::setVisibility($path, 'public');
+//        $link_file = URL::to('/').Storage::url('report/'.$date.'/'.$name_file);
+        Storage::setVisibility($path, 'public');// Đặt quyền truy cập cho file trong hệ thống tệp tin Laravel
+        $link_file = Storage::url($path); // Lấy đường dẫn truy cập tới file
         return response()->json(['path'=>$path,'downloadLink'=>$link_file]);
     }
 
