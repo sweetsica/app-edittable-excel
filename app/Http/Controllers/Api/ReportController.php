9cc163052a175cd690aca56adfa8cda686e0793b
Snow Deep <barondeepsnow@gmail.com>
2023-08-09 11:15:19 +0700

    change store2

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -26,16 +26,7 @@ class ReportController extends Controller
         $date = Carbon::today()->format('d-m-Y');
         $path = Storage::putFileAs("public/report/".'09-09-09-09',$request->file('files'),$name_file);
         $link_file = URL::to('/').Storage::url('report/'.$date.'/'.$name_file);
-//        if (file_exists($path)) {
-//            chmod('$path', 755);
-//        } else {
-//            // handle the error
-//        }
-//        exec('pwd', $output, $return_var);
         exec('cd .. && chmod -R 775 storage/app/public/report', $output, $return_var);
-//        exec('chmod -R 775 /home4/sweetsic/public_html/supapp7/public/storage', $output, $return_var);
-//            exec('cd ', $output, $return_var);
-//        shell_exec('chmod -R 775 storage/app/public/report');
 
         return response()->json(['path'=>$path,'downloadLink'=>$link_file, 'output' => $output]);
     }
