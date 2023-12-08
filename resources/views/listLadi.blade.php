2d6fe40d9a4a831f06c4ec6cc6107fbf8753742c
Snow Deep <barondeepsnow@gmail.com>
2023-12-08 14:13:09 +0700

    ladi api

--- resources/views/listLadi.blade.php
+++ resources/views/listLadi.blade.php
@@ -6,7 +6,7 @@
 <body>
 <?php
     foreach ($data as $data){
-        var_dump(json_decode($data));
+        var_dump(json_decode($data,200, ['Content-type'=> 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE));
     }
 ?>
 </body>
